5486b24e8bce23ff8e6d73bc2c09644bf6928fc3
Joseph Hamilton <jlhamilton0408@gmail.com>
2024-06-16 18:11:43 -0500

    Added SFML... Didn't ended up working

---
+++ SFML (not working)/README.md
@@ -0,0 +1,3 @@
+# SFML.NET Support (NOT WORKING)
+
+I started working on this, but the assemblies didn't correctly load with the interpreter
---
+++ SFML (not working)/SFML_EZ/Main.cs
@@ -0,0 +1,52 @@
+﻿using static EZCodeLanguage.EZHelp;
+using SFML;
+using SFML.Window;
+
+namespace SFML_EZ
+{
+    public static class Main
+    {
+        public static Window CreateWindow(object _width, object _height, object _title)
+        {
+            try
+            {
+                var x = GetParameter<int>(_width);
+                var y = GetParameter<int>(_height);
+                var title = GetParameter<string>(_title);
+                var videoMode = new VideoMode((uint)x, (uint)y);
+                var window = new Window(videoMode, title);
+                window.Closed += Window_Closed;
+                return window;
+            }
+            catch (Exception ex)
+            {
+                throw ThrowError(ex);
+            }
+        }
+        public static bool WindowIsOpen(object _window)
+        {
+            try
+            {
+                var window = GetParameter<Window>(_window);
+                return window.IsOpen;
+            }
+            catch (Exception ex)
+            {
+                throw ThrowError(ex);
+            }
+        }
+
+        private static void Window_Closed(object? sender, EventArgs e)
+        {
+            try
+            {
+                var window = (Window)sender;
+                window.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ThrowError(ex);
+            }
+        }
+    }
+}
